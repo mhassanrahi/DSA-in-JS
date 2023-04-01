5c507b08112e84f2d9610dac984ffa000b41aaa4
mhassanrahi <m.hassan.rahi@gmail.com>
2023-04-02 03:48:03 +0500

    * Symmetric void

---
+++ patterns/symmetricVoid.js
@@ -0,0 +1,38 @@
+const input = 5;
+let string = "";
+
+const printPattern = () => {
+  for (let i = 0; i < input; i++) {
+    for (let j = 1; j <= input - i; j++) {
+      string += "*";
+    }
+
+    for (let j = 1; j <= i * 2; j++) {
+      string += " ";
+    }
+
+    for (let j = 1; j <= input - i; j++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+
+  for (let i = 0; i < input; i++) {
+    for (let j = 0; j <= i; j++) {
+      string += "*";
+    }
+
+    for (let j = 0; j < (input - (i+1)) * 2; j++) {
+      string += " ";
+    }
+
+    for (let j = 0; j <= i; j++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+
+  return string;
+};
+
+console.log(printPattern());
